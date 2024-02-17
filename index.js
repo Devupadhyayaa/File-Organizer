165441557c071eac6828fe0ffc2b201bc032f6a7
Devansh Upadhyay <92574975+Devupadhyayaa@users.noreply.github.com>
2024-02-17 17:02:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const fs = require("fs/promises");
+const dirpath = "/home/devansh/Pictures/Screenshots";
+
+async function readdirectory(){
+    try {
+        const files = await fs.readdir(dirpath);
+        return files;
+    } 
+    catch (err) {
+        console.log("Error is", err.message);
+        throw err;
+    }
+}
+
+(async ()=>{
+    try{
+        const allfiles = await readdirectory();
+        // create folders with the name of extensions
+        const hash={};
+        for(str of allfiles){
+            const frequency = str.split('.').length - 1;
+            if(frequency==0||frequency>1)continue;
+            var sufix = str.split('.').pop();
+            if (!hash.hasOwnProperty(sufix)) {
+                hash[sufix] = 0;
+            }
+            hash[sufix]++;
+            if(sufix!='json'&&hash[sufix]===1&&sufix!='js'){
+                const newpth = `${dirpath}/${sufix}`;
+                await fs.mkdir(newpth);
+            }
+        }
+        //  now lets move the files 
+        for(str of allfiles){
+            const frequency = str.split('.').length - 1;
+            if(frequency==0||frequency>1)continue;
+            var sufix = str.split('.').pop();
+            if(sufix!='json'&&sufix!='js'){
+                const oldpth = `${dirpath}/${str}`;
+                const newpath = `${dirpath}/${sufix}/${str}`;
+                await fs.rename(oldpth,newpath);
+            }
+        }
+    }
+    catch (e){
+        throw e;
+    }
+})();
